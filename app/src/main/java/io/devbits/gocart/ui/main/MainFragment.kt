76ee2452f135cfca990a9212373f7c218c87437c
Eton Otieno <etonotieno@gmail.com>
2020-12-19 21:10:53 +0300

    Initial commit

---
+++ app/src/main/java/io/devbits/gocart/ui/main/MainFragment.kt
@@ -0,0 +1,32 @@
+package io.devbits.gocart.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import io.devbits.gocart.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/io/devbits/gocart/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package io.devbits.gocart.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
