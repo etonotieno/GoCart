be579d71e1ab7a3b5317b0a20702663b80f2ef41
Eton Otieno <etonotieno@gmail.com>
2023-08-29 13:17:18 +0300

    Set up Static Code Analysis

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,41 @@ plugins {
     id("com.android.application") version "8.1.0" apply false
     id("com.android.library") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.23.1"
+    id("com.diffplug.spotless") version "6.21.0"
+}
+
+subprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+    detekt {
+        config.setFrom("${project.rootDir}/detekt.yml")
+        parallel = true
+        buildUponDefaultConfig = true
+    }
+
+    apply(plugin = "com.diffplug.spotless")
+    spotless {
+        kotlin {
+            target("**/*.kt")
+            targetExclude("**/build/**/*.kt")
+            ktlint("0.50.0").userData(mapOf("android" to "true"))
+            licenseHeaderFile(rootProject.file("spotless/copyright.kt"))
+            trimTrailingWhitespace()
+            endWithNewline()
+        }
+        format("kts") {
+            target("**/*.kts")
+            targetExclude("**/build/**/*.kts")
+            licenseHeaderFile(rootProject.file("spotless/copyright.kts"), "(^(?![\\/ ]\\*).*$)")
+        }
+        format("xml") {
+            target("**/*.xml")
+            targetExclude("**/build/**/*.xml")
+            licenseHeaderFile(rootProject.file("spotless/copyright.xml"), "(<[^!?])")
+        }
+    }
+
+    dependencies {
+        detektPlugins("io.nlopez.compose.rules:detekt:0.2.1")
+    }
 }
---
+++ staticChecks.bat
@@ -0,0 +1,2 @@
+@echo off
+.\gradlew detekt && .\gradlew spotlessApply
