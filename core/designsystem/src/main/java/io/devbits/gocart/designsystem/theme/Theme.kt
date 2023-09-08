1ce23a91303b77421955bdfe8b0f260760400c72
Eton Otieno <etonotieno@gmail.com>
2023-09-08 10:22:01 +0300

    Add dark theme

--- core/designsystem/src/main/java/io/devbits/gocart/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/io/devbits/gocart/designsystem/theme/Theme.kt
@@ -15,9 +15,16 @@
  */
 package io.devbits.gocart.designsystem.theme
 
+import android.os.Build
+import androidx.annotation.ChecksSdkIntAtLeast
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
 private val lightColorScheme = lightColorScheme(
     primary = md_theme_light_primary,
@@ -51,12 +58,62 @@ private val lightColorScheme = lightColorScheme(
     scrim = md_theme_light_scrim,
 )
 
+private val darkColorScheme = darkColorScheme(
+    primary = md_theme_dark_primary,
+    onPrimary = md_theme_dark_onPrimary,
+    primaryContainer = md_theme_dark_primaryContainer,
+    onPrimaryContainer = md_theme_dark_onPrimaryContainer,
+    secondary = md_theme_dark_secondary,
+    onSecondary = md_theme_dark_onSecondary,
+    secondaryContainer = md_theme_dark_secondaryContainer,
+    onSecondaryContainer = md_theme_dark_onSecondaryContainer,
+    tertiary = md_theme_dark_tertiary,
+    onTertiary = md_theme_dark_onTertiary,
+    tertiaryContainer = md_theme_dark_tertiaryContainer,
+    onTertiaryContainer = md_theme_dark_onTertiaryContainer,
+    error = md_theme_dark_error,
+    errorContainer = md_theme_dark_errorContainer,
+    onError = md_theme_dark_onError,
+    onErrorContainer = md_theme_dark_onErrorContainer,
+    background = md_theme_dark_background,
+    onBackground = md_theme_dark_onBackground,
+    surface = md_theme_dark_surface,
+    onSurface = md_theme_dark_onSurface,
+    surfaceVariant = md_theme_dark_surfaceVariant,
+    onSurfaceVariant = md_theme_dark_onSurfaceVariant,
+    outline = md_theme_dark_outline,
+    inverseOnSurface = md_theme_dark_inverseOnSurface,
+    inverseSurface = md_theme_dark_inverseSurface,
+    inversePrimary = md_theme_dark_inversePrimary,
+    surfaceTint = md_theme_dark_surfaceTint,
+    outlineVariant = md_theme_dark_outlineVariant,
+    scrim = md_theme_dark_scrim,
+)
+
 @Composable
-fun GoCartTheme(content: @Composable () -> Unit) {
+fun GoCartTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    disableDynamicTheming: Boolean = false,
+    dynamicTheme: Boolean = supportsDynamicTheming(),
+    content: @Composable () -> Unit,
+) {
+    val colorScheme = when {
+        !disableDynamicTheming && dynamicTheme -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> darkColorScheme
+        else -> lightColorScheme
+    }
+
     MaterialTheme(
-        colorScheme = lightColorScheme,
+        colorScheme = colorScheme,
         typography = Typography,
         shapes = Shapes,
         content = content,
     )
 }
+
+@ChecksSdkIntAtLeast(api = Build.VERSION_CODES.S)
+fun supportsDynamicTheming() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
