27830da8bcce05e15a8e3865ce33e6bc254a1363
Eton Otieno <etonotieno@gmail.com>
2023-09-13 09:59:18 +0300

    Add Coil GIF dependency

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -70,6 +70,8 @@ dependencies {
 
     api("androidx.navigation:navigation-compose:2.7.0")
 
+    api("io.coil-kt:coil-compose:2.4.0")
+
     testImplementation("junit:junit:4.13.2")
 
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
