61835f5c17e53abdcef6cc4a65da6933cd4756be
Eton Otieno <etonotieno@gmail.com>
2023-09-20 15:11:05 +0300

    Refactor GoCart TopAppBar

--- core/designsystem/src/main/java/io/devbits/gocart/designsystem/component/AppBar.kt
+++ core/designsystem/src/main/java/io/devbits/gocart/designsystem/component/AppBar.kt
@@ -16,6 +16,7 @@
 package io.devbits.gocart.designsystem.component
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -32,6 +33,9 @@ import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -51,8 +55,8 @@ fun GoCartTopAppBar(
     onCheckout: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    TopAppBar(
-        modifier = modifier.fillMaxWidth(),
+    GCTopAppBar(
+        modifier = modifier,
         title = {
             Image(
                 painter = painterResource(id = resourcesR.drawable.ic_go_cart_horizontal_color),
@@ -65,7 +69,6 @@ fun GoCartTopAppBar(
                 Icon(
                     imageVector = Icons.Filled.Menu,
                     contentDescription = null,
-                    tint = MaterialTheme.colorScheme.primary,
                 )
             }
         },
@@ -84,6 +87,30 @@ fun GoCartTopAppBar(
                 )
             }
         },
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GCTopAppBar(
+    title: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    navigationIcon: @Composable () -> Unit = {},
+    actions: @Composable (RowScope.() -> Unit) = {},
+    windowInsets: WindowInsets = TopAppBarDefaults.windowInsets,
+    colors: TopAppBarColors = TopAppBarDefaults.topAppBarColors(
+        navigationIconContentColor = MaterialTheme.colorScheme.primary,
+    ),
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+) {
+    TopAppBar(
+        modifier = modifier.fillMaxWidth(),
+        title = title,
+        navigationIcon = navigationIcon,
+        actions = actions,
+        windowInsets = windowInsets,
+        colors = colors,
+        scrollBehavior = scrollBehavior,
     )
 }
 
