87ae1a2aca8658b20670113154a94891d0d5adce
Eton Otieno <etonotieno@gmail.com>
2023-09-21 11:41:50 +0300

    Create ProductDetailsCard component

---
+++ core/designsystem/src/main/java/io/devbits/gocart/designsystem/component/ProductDetailsCard.kt
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2023 Eton Otieno
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.devbits.gocart.designsystem.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import io.devbits.gocart.designsystem.model.Product
+import io.devbits.gocart.designsystem.theme.GoCartTheme
+
+@Composable
+fun ProductDetailsCard(
+    product: Product,
+    modifier: Modifier = Modifier,
+) {
+    var quantity by remember { mutableIntStateOf(0) }
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+    ) {
+        Column(
+            modifier = Modifier.weight(1f),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            Text(
+                text = product.name,
+                style = MaterialTheme.typography.titleSmall,
+                fontWeight = FontWeight.Bold,
+            )
+
+            val productQuantity = buildString {
+                if (product.unit.isNotBlank()) append(product.unit)
+                append(" ")
+                if (product.unitQuantity.isNotBlank()) append("Approx. ${product.unitQuantity}")
+            }
+            Text(
+                text = productQuantity,
+                style = MaterialTheme.typography.bodySmall,
+            )
+
+            Text(
+                text = "KSH ${product.price} ",
+                style = MaterialTheme.typography.titleMedium,
+            )
+        }
+
+        QuantityControl(
+            quantity = quantity,
+            onAdd = { quantity++ },
+            onRemove = {
+                if (quantity > 0) quantity--
+            },
+            showDelete = true,
+            modifier = Modifier.align(Alignment.Top),
+        )
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
+@Composable
+private fun ProductDetailsCardPreview() {
+    GoCartTheme {
+        ProductDetailsCard(product = sampleProducts[0])
+    }
+}
