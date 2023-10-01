f0130d2e765917ba91dd4c1c3afd23034c277972
Eton Otieno <etonotieno@gmail.com>
2023-10-01 13:57:50 +0300

    Add multiplatform modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.detekt)
